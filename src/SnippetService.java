c00d15ed1a6a93e703b2c8f68483a3108085fb0a
visualtrash <42218536+visualtrash@users.noreply.github.com>
2019-06-20 19:11:05 +0300

    Update SnippetService.java

--- src/SnippetService.java
+++ src/SnippetService.java
@@ -6,18 +6,32 @@ public class SnippetService {
     private List<Snippet> listOfSnippets = new ArrayList<>();
 
     public Object add(String name, String text) {
-        return new Object();
+
+        Snippet snippet = new Snippet("snippet","someCode");
+        listOfSnippets.add(snippet);
+        return snippet;
     }
 
-    public List find(String searchString) {
-        return null;
+    public Snippet find(String searchString) {
+        Snippet result = null;
+        for (int i = 0; i < listOfSnippets.size(); i++) {
+            if (listOfSnippets.contains(searchString)) {
+                result = listOfSnippets.get(i);
+            }
+        }
+        return result;
     }
 
     public boolean update(UUID id, String name, String text) {
+        for (int i = 0; i < listOfSnippets.size(); i++) {
+            if (id.equals()
+        }
         return true;
     }
 
     public boolean remove(UUID id) {
         return true;
     }
+    
 }
+

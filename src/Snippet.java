503b88768be8e1a63a21071c788544a0f2fe9441
visualtrash <42218536+visualtrash@users.noreply.github.com>
2019-06-20 19:16:02 +0300

    Update Snippet.java

--- src/Snippet.java
+++ src/Snippet.java
@@ -7,8 +7,30 @@ public class Snippet {
     private Date date = new Date();
     private UUID id = UUID.randomUUID();
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    
     Snippet (String name, String text) {
         this.name = name;
         this.text= text;
     }
 }
+

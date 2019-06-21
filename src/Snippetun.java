8eff6da0d913c5851f545a7e96ef0dd51b1d242e
visualtrash <tarantinoumer@gmail.com>
2019-06-21 14:48:04 +0300

    add functional method "add"

--- src/Snippetun.java
+++ src/Snippetun.java
@@ -16,22 +16,6 @@ public class Snippetun {
             System.out.println("enter the text of snippet");
             String textUserSnippet = reader.readLine();
             snippetService.add(nameUserSnippet, textUserSnippet);
-        } else if (userCommand.toUpperCase().equals("find".toUpperCase())) {        //find
-            System.out.println("enter the keyword or something");
-            String searchField = reader.readLine();
-            snippetService.find(searchField);
-        } else if (userCommand.toUpperCase().equals("update".toUpperCase())) {      //update ??
-            System.out.println("what do u want to update? (NAME / TEXT)");
-            String userUpdateChoise = reader.readLine();
-            if (userUpdateChoise.toUpperCase().equals("NAME")) {
-                System.out.println("enter new name");
-                String newName = reader.readLine();
-
-            } else if (userUpdateChoise.toUpperCase().equals("TEXT")) {
-
-            } else System.out.println("wrong command");
-        } else if (userCommand.toUpperCase().equals("REMOVE")) {        //remove ??
-            System.out.println();
         }
 
     }

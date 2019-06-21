e537400afe52197d2946baede2b22ccf65dd52f8
visualtrash <tarantinoumer@gmail.com>
2019-06-21 14:46:12 +0300

    add functional methods

--- src/Snippetun.java
+++ src/Snippetun.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class Snippetun {
-    public static void main(String[] args) {
-        System.out.println("App started");
+    public static void main(String[] args) throws Exception {
+        SnippetService snippetService = new SnippetService();
+
+        System.out.println("please enter the command");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String userCommand = reader.readLine();     //ввод команды юзера
+
+        if (userCommand.toUpperCase().equals("add".toUpperCase())) {        //add
+            System.out.println("enter the name of snippet");
+            String nameUserSnippet = reader.readLine();
+            System.out.println("enter the text of snippet");
+            String textUserSnippet = reader.readLine();
+            snippetService.add(nameUserSnippet, textUserSnippet);
+        } else if (userCommand.toUpperCase().equals("find".toUpperCase())) {        //find
+            System.out.println("enter the keyword or something");
+            String searchField = reader.readLine();
+            snippetService.find(searchField);
+        } else if (userCommand.toUpperCase().equals("update".toUpperCase())) {      //update ??
+            System.out.println("what do u want to update? (NAME / TEXT)");
+            String userUpdateChoise = reader.readLine();
+            if (userUpdateChoise.toUpperCase().equals("NAME")) {
+                System.out.println("enter new name");
+                String newName = reader.readLine();
+
+            } else if (userUpdateChoise.toUpperCase().equals("TEXT")) {
+
+            } else System.out.println("wrong command");
+        } else if (userCommand.toUpperCase().equals("REMOVE")) {        //remove ??
+            System.out.println();
+        }
+
     }
 }

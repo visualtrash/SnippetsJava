fc213230eb80bb251267edb5329d05a64a6747f1
visualtrash <tarantinoumer@gmail.com>
2019-06-23 00:27:30 +0300

    update methods

--- src/SnippetService.java
+++ src/SnippetService.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 public class SnippetService {
 
-    private List<Snippet> listOfSnippets = new ArrayList<>();
+    private List<Snippet> listOfSnippets;
 
     public Snippet add(String name, String text) {
         Snippet snippet = new Snippet(name, text);
@@ -16,16 +16,27 @@ public class SnippetService {
     }
 
     /**
+     * @param filePath путь к файлу из которого нужно загрузить список снипетов
+     */
+    public SnippetService(String filePath) {
+        listOfSnippets = loadSnippetsFromDisc(filePath);
+    }
+
+    // todo написать для этого метода команду в консоли
+
+    /**
      * Найти снипеты в списке, по тексту в их названии
+     *
      * @param searchString строка, которую должно включаться в себя название искомого снипета
-     * @return*/
+     * @return
+     */
     public List<Snippet> find(String searchString) {
         // список снипетов, которые мы нашли по имени
         List<Snippet> findedSnippets = new ArrayList<>();
 
         for (Snippet currentSnippet : listOfSnippets) {
             // мы взяли текущий снипет из списка, конвертировали его имя в Upper Case и смотрим включает ли оно строку поиска, которая тоже конвертирована в Upper Case
-            if (currentSnippet.getName().toUpperCase().contains(searchString.toUpperCase())){
+            if (currentSnippet.getName().toUpperCase().contains(searchString.toUpperCase())) {
                 findedSnippets.add(currentSnippet);
             }
         }
@@ -33,21 +44,58 @@ public class SnippetService {
         return findedSnippets;
     }
 
-    public void update(UUID id, String name, String text) throws Exception {
-         // флаг, обозначающий был ли найден сниппет в списке
-         boolean snippetWasFounded = false;
+    /**
+     * @return все добавленные снипеты
+     */
+    public void getAll() {
+        for (Snippet snippetOfList : listOfSnippets) {
+            System.out.println("NAME: " + snippetOfList.getName());
+            System.out.println("--------------------------------");
+            System.out.println("ID: " + snippetOfList.getId());
+            System.out.println("--------------------------------");
+            System.out.println("TEXT: " + snippetOfList.getText());
+        }
+
+    }
+
+    // todo разбить на 2 метода - 1) обновить имя снипета 2) обновиить текст снипета
+    // todo для них сделать 2 разные команды для консоли
+    //изменение имени
+    public void updateName(UUID id, String name, String text) throws Exception {
+        // флаг, обозначающий был ли найден сниппет в списке
+        boolean snippetWasFounded = false;
 
         for (Snippet currentSnippet : listOfSnippets) {
-            if (currentSnippet.getId().equals(id)){
+            if (currentSnippet.getId().equals(id)) {
                 snippetWasFounded = true;
 
                 currentSnippet.setName(name);
+
+                break;
+            }
+        }
+
+        if (!snippetWasFounded) {
+            throw new Exception("Cannot find snippet for ID = " + id);
+        }
+    }
+
+    // изменение текста
+    public void updateText(UUID id, String name, String text) throws Exception {
+        // флаг, обозначающий был ли найден сниппет в списке
+        boolean snippetWasFounded = false;
+
+        for (Snippet currentSnippet : listOfSnippets) {
+            if (currentSnippet.getId().equals(id)) {
+                snippetWasFounded = true;
+
+
                 currentSnippet.setText(text);
                 break;
             }
         }
 
-        if (!snippetWasFounded){
+        if (!snippetWasFounded) {
             throw new Exception("Cannot find snippet for ID = " + id);
         }
     }
@@ -57,7 +105,7 @@ public class SnippetService {
         boolean snippetWasFounded = false;
 
         for (Snippet currentSnippet : listOfSnippets) {
-            if (currentSnippet.getId().equals(id)){
+            if (currentSnippet.getId().equals(id)) {
                 snippetWasFounded = true;
 
                 listOfSnippets.remove(currentSnippet);
@@ -65,9 +113,22 @@ public class SnippetService {
             }
         }
 
-        if (!snippetWasFounded){
+        if (!snippetWasFounded) {
             throw new Exception("Cannot find snippet for ID = " + id);
         }
+    }
+
+    /**
+     * Сохранить коллекцию снипетов в файл на диск
+     */
+    public void persist() {
+    }
+
+    /**
+     * Инициализировать коллекцию снипетов считав сериализованный список снипетов из файла
+     */
+    private List<Snippet> loadSnippetsFromDisc(String filePath) {
+        return new ArrayList<>();
     }
 
 }
